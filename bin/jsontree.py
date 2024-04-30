172a774fb7f5d37f82fc12f1e4b984e8a39be5bd
queelius <lex@metafunctor.com>
2024-04-30 12:40:39 -0500

    updates

--- bin/jsontree.py
+++ bin/jsontree.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import json, argparse, sys, logging, textwrap
-from dicttree import DictTree
+from treekit.dicttree import DictTree
 
 def show_json_spec():
     print(textwrap.dedent(f'''
