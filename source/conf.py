54309bc4f519b0208ba0cbb9878d06ecf2cbfc63
queelius <lex@metafunctor.com>
2024-08-25 02:24:40 -0500

    updates

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,9 @@ templates_path = ['_templates']
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
+
 
 html_static_path = ['_static', 'images']
 
